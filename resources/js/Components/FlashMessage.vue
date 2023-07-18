6d1ce5183984334c06a35e7b54dd0bf8e906e2aa
近藤一 <>
2023-07-19 00:24:06 +0900

    商品関連

---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div v-if="$page.props.flash.status ==='success'"
+     class="bg-blue-300 p-4 text-white">
+        {{ $page.props.flash.message }}
+    </div>
+    <div v-if="$page.props.flash.status === 'danger'"
+        class="bg-red-300 p-4 text-white">
+        {{ $page.props.flash.message }}
+    </div>  
+</template>
